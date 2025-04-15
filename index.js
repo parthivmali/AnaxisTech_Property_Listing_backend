dac72edba2456f1b61986dafcbe502a658da9c06
Parthiv Mali <parthivm@rbi.edu.in>
2025-04-15 21:46:56 +0530

    Update index.js
    Changes completed

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ import { PORT } from './config/env.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // In case you use POST later
+app.use(express.json());
 
-// Routes
 app.use('/properties', propertyRoutes);
 
-// Root route
 app.get('/', (req, res) => {
   res.send('🏠 RealtyHub API is running...');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
